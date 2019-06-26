c94c0531b41ee01f4b5d1a4d65be504f64a96e68
cloud-mouse <286986069@qq.com>
2019-06-26 13:17:53 +0800

    车贝健

--- src/config/index.js
+++
@@ -1,4 +0,0 @@
-import './element-ui';
-import './vue-echarts';
-// 缺少依赖，暂时搁置
-// import './bootstrap-vue';
--- src/router/404.js
+++
@@ -1,8 +0,0 @@
-const route = [
-  {
-    path: '*',
-    redirect: { path: '/pages/404' },
-  },
-];
-
-export default route;
